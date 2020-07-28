b66471317f5c178cb67419f4dd3cc444c6f64056
Jonathan Medina <jonathanmedhed@gmail.com>
2020-07-28 14:27:52 -0400

    remove auth from upload route

--- routes/api/users.js
+++ routes/api/users.js
@@ -88,7 +88,8 @@ aws.config.region = 'us-east-2';
 // @route   GET api/users/sign-s3
 // @desc    Get upload img and get url
 // @access  Public
-router.get('/sign-s3', auth, async (req, res) => {
+router.get('/sign-s3', async (req, res) => {
+	console.log('Route running');
 	process.env.AWS_ACCESS_KEY_ID = 'AKIAJRQ5SEZNKOHKM2NA';
 	process.env.AWS_SECRET_ACCESS_KEY = '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';
 	process.env.S3_BUCKET = 'myonlineshopvzla';
