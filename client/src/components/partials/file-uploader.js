1227d75d51b5385c42022d5ef2079567dcc8e23d
Jonathan Medina <jonathanmedhed@gmail.com>
2020-07-28 14:46:43 -0400

    remove auth from upload route 2

--- client/src/components/partials/file-uploader.js
+++ client/src/components/partials/file-uploader.js
@@ -81,6 +81,7 @@ const UploadComp = ({ auto, multiple, setAlert, setSuccess, setCurrentUser, uplo
 		xhr.onreadystatechange = () => {
 			if (xhr.readyState === 4) {
 				if (xhr.status === 200) {
+					console.log('Working');
 					const response = JSON.parse(xhr.responseText);
 					uploadFile(file, response.signedRequest, response.url);
 				} else {
